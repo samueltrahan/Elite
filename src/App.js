beefc2949b609de0c86869b8687492359cc26122
Samuel Trahan <samtrahan91@gmail.com>
2020-11-02 19:47:33 -0600

    banner component add and styling some what

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
+import Banner from './components/Banner/Banner';
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Banner />
     </div>
   );
 }
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Banner.css';
+
+export default function Banner() {
+  return (
+    <section>
+      <div className="bannder-heading">
+        <header className="banner-header">
+          <div className="elite">
+            <h1>Elite & React</h1>
+          </div>
+        </header>
+      </div>
+    </section>
+  );
+}
