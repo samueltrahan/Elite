c461bdac3a11572906853bfda8db1a184e81bcf6
Samuel Trahan <samtrahan91@gmail.com>
2020-11-03 09:10:05 -0600

    cards and footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Banner from './components/Banner/Banner';
+import Footer from './components/Footer/Footer';
+import Cards from './components/Cards/Cards';
 
 export default function App() {
   return (
     <div>
       <Header />
       <Banner />
+      <Cards />
+      <Footer />
     </div>
   );
 }
