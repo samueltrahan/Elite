2f8c2cbcd2903feba5f00278087f421ecfb3ece4
Samuel Trahan <samtrahan91@gmail.com>
2020-11-03 12:27:05 -0600

    card information and footer styling

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -14,9 +14,21 @@ export default function App() {
       <Header />
       <Banner />
       <div className="card-position">
-        <Cards image={Pic01} title="About Elite & React" />
-        <Cards image={Pic02} title="Welcome to Elite & React" />
-        <Cards image={Pic03} title="Getting Started with Elite & React" />
+        <Cards
+          image={Pic01}
+          title="About Elite & React"
+          paragraph="Elite is a React application template that can be used to display projects or information..."
+        />
+        <Cards
+          image={Pic02}
+          title="Welcome to Elite & React"
+          paragraph="Check out the process it took to build this application and some of the features that you need to know to customize this site."
+        />
+        <Cards
+          image={Pic03}
+          title="Getting Started with Elite & React"
+          paragraph="Welcome! I know you're looking for a site to display your work.  Check out this section for some details on how to get started and things you can do to make this site your own."
+        />
       </div>
       <Footer />
     </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './Footer.css';
 
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="footer">
+      <div className="copy-right">&copy; 2020 Cinque Web Development</div>
+      <div className="footer-links">
+        <a className="foot" href="/">
+          Home
+        </a>
+        &nbsp;&nbsp;
+        <a className="foot" href="/tag">
+          Tag
+        </a>
+        &nbsp;&nbsp;
+        <a className="foot" href="/author">
+          Author
+        </a>
+        &nbsp;&nbsp;
+        <a className="foot" href="/about">
+          About
+        </a>
+        &nbsp;&nbsp;
+      </div>
+    </footer>
   );
 }
