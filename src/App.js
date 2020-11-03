cd5cb3a22e427ae17b7f6c4135c758b681e694ff
Samuel Trahan <samtrahan91@gmail.com>
2020-11-03 15:28:32 -0600

    about page fix

--- src/App.js
+++ src/App.js
@@ -14,25 +14,33 @@ export default function App() {
   return (
     <Router>
       <Header />
-      <Banner />
-      <div className="card-position">
-        <Cards
-          image={Pic01}
-          path="/about"
-          title="About Elite & React"
-          paragraph="Elite is a React application template that can be used to display projects or information..."
-        />
-        <Cards
-          image={Pic02}
-          title="Welcome to Elite & React"
-          paragraph="Check out the process it took to build this application and some of the features that you need to know to customize this site."
-        />
-        <Cards
-          image={Pic03}
-          title="Getting Started with Elite & React"
-          paragraph="Welcome! I know you're looking for a site to display your work.  Check out this section for some details on how to get started and things you can do to make this site your own."
-        />
-      </div>
+      <Route
+        exact
+        path="/"
+        render={() => (
+          <>
+            <Banner />
+            <div className="card-position">
+              <Cards
+                image={Pic01}
+                path="/about"
+                title="About Elite & React"
+                paragraph="Elite is a React application template that can be used to display projects or information..."
+              />
+              <Cards
+                image={Pic02}
+                title="Welcome to Elite & React"
+                paragraph="Check out the process it took to build this application and some of the features that you need to know to customize this site."
+              />
+              <Cards
+                image={Pic03}
+                title="Getting Started with Elite & React"
+                paragraph="Welcome! I know you're looking for a site to display your work.  Check out this section for some details on how to get started and things you can do to make this site your own."
+              />
+            </div>
+          </>
+        )}
+      />
       <Footer />
       <Route exact path="/about" render={() => <AboutPage />} />
     </Router>
