b448882dd92019f0de47c5b00dc411962d9e6d9f
Samuel Trahan <samtrahan91@gmail.com>
2020-11-02 19:29:53 -0600

    header styling  finsihed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,6 +8,23 @@ export default function Header() {
         <a href="/">
           <img className="elite-logo" alt="" src="/images/elite.png" />
         </a>
+        <div className="social-links" style={{ color: 'red' }}>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/samueltrahan/"
+          >
+            <i className="fab fa-linkedin fa-2x" />
+          </a>
+          &nbsp;&nbsp;
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/samueltrahan/Elite"
+          >
+            <i className="fab fa-github fa-2x" />
+          </a>
+        </div>
       </nav>
     </header>
   );
