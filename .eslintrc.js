1a23d2e1b78b823970cc237a5ccf118461adae2e
Samuel Trahan <samtrahan91@gmail.com>
2020-11-02 18:38:39 -0600

    linter and prettier implemented

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'eslint-config-prettier',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'eslint-plugin-prettier'],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+      },
+    ],
+    'import/no-extraneous-dependencies': [0, false],
+    'import/prefer-default-export': [0, false],
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      { controlComponents: ['Dropdown', 'Input'] },
+    ],
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [0, false],
+    'react/jsx-one-expression-per-line': [0, false],
+    'react/prop-types': [0, false],
+    'react/react-in-jsx-scope': [0, false],
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+};
